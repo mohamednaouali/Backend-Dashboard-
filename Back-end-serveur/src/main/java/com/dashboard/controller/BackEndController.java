4e7d32490cc5da387a4a103ae55ccb96094def49
mohamednaouali <61143482+mohamednaouali@users.noreply.github.com>
2020-03-20 15:00:42 +0100

    Initial commit

---
+++ Back-end-serveur/src/main/java/com/dashboard/controller/BackEndController.java
@@ -0,0 +1,28 @@
+package com.dashboard.controller;
+
+import com.dashboard.aop.CONS;
+import com.netflix.appinfo.ApplicationInfoManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ */
+@RestController
+
+@RequestMapping("/welcome")
+public class BackEndController {
+     final Logger logger = (Logger) LoggerFactory.getLogger(BackEndController.class);
+    @RequestMapping(value = "/info", method = RequestMethod.GET)
+    @CONS
+    public String info() {
+       // return ("**** __Response From this Service__ ***");
+
+        return "welcome " + String.valueOf(ApplicationInfoManager.getInstance().getInfo().getPort()) + "  has responded";
+    }
+
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Dashboard-Monitoring
+ BackEnd Dashboard Monitoring Microservices #spring #cloud #stream #kafka #zookeeper #Eureka #Zuul #Cassandra 
